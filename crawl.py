2b8470504d03a7535742494399de36cad81db9ab
emmanuelbo13 <bustema90@gmail.com>
2026-01-09 20:59:57 -0500

    added functionality to extract html content

--- crawl.py
+++ crawl.py
@@ -1,4 +1,5 @@
 from urllib.parse import urlparse
+from bs4 import BeautifulSoup
 
 def normalize_url(absolute_uri):
     parsed_url = urlparse(absolute_uri)
@@ -8,10 +9,53 @@ def normalize_url(absolute_uri):
     normalized_url = f"{netloc}{path}{query}"
     
     return normalized_url
-
-# --- Manual Test ---
+# manual tests:
 test_url = "https://blog.boot.dev/path"
-
 expected_url = "blog.boot.dev/path"
+#print(normalize_url(test_url))
 
-print(normalize_url(test_url))
+# Function to extract elements from html
+def get_h1_from_html(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    title = soup.find('h1')
+    return title.get_text() if title else None
+
+def get_first_paragraph_from_html(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    first_paragraph = soup.find_all('p')[0].get_text()
+    return first_paragraph
+# manual test:
+html_test = "<html><body><h1>Test Title</h1><br><p>Hello world</p><p>Hello You</p></body></html>"
+html_test2 = '''<html><body>
+                <h1>Welcome to Boot.dev</h1>
+                <main>
+                <p>Learn to code by building real projects.</p>
+                <p>This is the second paragraph.</p>
+                </main>
+            </body></html>'''
+
+# print(get_h1_from_html(html_test2))
+# print(get_first_paragraph_from_html(html_test2))
+
+# Get content from file 
+
+with open('index.html', 'r') as f:
+    html_content = f.read()
+
+print(html_content)
+
+
+# class PageScraper:
+#     def __init__(self, html):
+#         self.soup = BeautifulSoup(html, 'html.parser')
+    
+#     def get_h1(self):
+#         return self.soup.find('title').get_text()
+    
+#     def get_first_paragraph(self):
+#         return self.soup.find_all('p')[0].get_text()
+
+# # Usage:
+# scraper = PageScraper(html_test2)
+# print(scraper.get_h1())
+# print(scraper.get_first_paragraph())
--- test_crawl.py
+++ test_crawl.py
@@ -1,5 +1,5 @@
 import unittest
-from crawl import normalize_url
+from crawl import normalize_url, get_h1_from_html, get_first_paragraph_from_html
 
 class TestCrawl(unittest.TestCase):
     def test_normalize_url(self):
@@ -7,6 +7,30 @@ class TestCrawl(unittest.TestCase):
         actual = normalize_url(input_url)
         expected = "blog.boot.dev/path"
         self.assertEqual(actual, expected)
+
+    def test_get_h1_from_html(self):
+        html = """<html><body>
+                <h1>Welcome to Boot.dev</h1>
+                <main>
+                <p>Learn to code by building real projects.</p>
+                <p>This is the second paragraph.</p>
+                </main>
+            </body></html>"""
+        actual = get_h1_from_html(html)
+        expected = "Welcome to Boot.dev"
+        self.assertEqual(actual, expected)
+
+    def test_get_first_paragraph_from_html(self):
+        html = """<html><body>
+                <h1>Welcome to Boot.dev</h1>
+                <main>
+                <p>Learn to code by building real projects.</p>
+                <p>This is the second paragraph.</p>
+                </main>
+            </body></html>"""
+        actual = get_first_paragraph_from_html(html)
+        exptected = "Learn to code by building real projects."
+        self.assertEqual(actual, exptected)
 
 if __name__ == "__main__":
     unittest.main()
