cee5b1b043cf98fc8da575ceb1922e24da33183a
emmanuelbo13 <bustemma90@gmail.com>
2026-01-11 01:52:11 -0500

    complete get_urls from_html function

--- crawl.py
+++ crawl.py
@@ -20,3 +20,29 @@ def get_first_paragraph_from_html(html):
     first_paragraph = soup.find_all('p')[0].get_text()
     return first_paragraph
 
+def get_urls_from_html(html, base_url):
+    soup = BeautifulSoup(html, 'html.parser')
+    anchor_elements = soup.find_all('a')
+    return [anchor_elements]
+
+with open('index.html', 'r') as f:
+    html_content = f.read()
+
+absolute_url = "https://blog.boot.dev"
+relative_url = "/posts/1"
+
+soup = BeautifulSoup(html_content, 'html.parser')
+print(soup.find_all('a'))
+print(soup.find('a').get('href'))
+
+print(urljoin(absolute_url, relative_url))
+
+def is_absolute(url):
+    """Check if a URL is absolute by verifying the scheme and network location."""
+    parsed_url = urlparse(url)
+    return bool(parsed_url.scheme and parsed_url.netloc)
+
+if is_absolute(relative_url):
+    print(relative_url)
+else:
+    absolute = urljoin(absolute_url, relative_url)
