f49aa2f9a55ef35e57482dddb17576c4935101dc
emmanuelbo13 <bustemma90@gmail.com>
2026-01-13 22:16:17 -0500

    sys library event handling

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from web-scrapper!")
+import sys
 
+def main():
+
+    argc = len(sys.argv)
+    if argc < 2:
+        print("no website provided")
+        sys.exit(1)
+    elif argc > 2:
+        print("too many arguments provided")
+        #print(sys.argv)
+        sys.exit(1)
+    
+    base_url = sys.argv[1]
+    print("starting crawl for: ", base_url)
 
 if __name__ == "__main__":
     main()
